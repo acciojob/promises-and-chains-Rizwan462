d6c2a205bfa18b3185a8b13bf23574f91b244c47
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-08-31 23:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+let userForm = document.getElementById('userForm');
+let submit = document.getElementById('submit');
+
+submit.addEventListener("click", (e)=>{
+	event.preventDefault();
+
+	const name = document.getElementById('name').value;
+	const age = document.getElementById('age').value;
+
+	if(name === '' || age === '') {
+		alert('please fill in both fields.');
+		return;
+	}	
+
+const ageNum = pareInt(age);
+
+const ageCheck = new Promise((res, rej)=>{
+	setTimeout(()=>{
+		if(ageNum > 18) {
+			res();
+		}else{
+			rej();
+		}
+	}, 4000);
+})
+ageCheck
+	.then(()=>{
+		alert(`Welcome, ${name}. You can vote.`);
+	})
+	.catch(()=>{
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+	});
+});
+
+
+
+
+		
